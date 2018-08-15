391b50308c2eb3c1abfa2556ef44e414bc09e86d
Meguru Yasuda <yasudamel@gmail.com>
2018-08-14 19:12:17 -0700

    fix the logic for factorial

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $("form#numberinput").submit(function(event){
     event.preventDefault();
-    var result = 1;
     var numberInput = parseInt($("#number").val());
+    var result = 1;
     for (var index = numberInput; index >= 1; index -= 1 ) {
       result*=index;
-      console.log(result);
       }
+      $("#result").append(" " + result);
   });
 });
